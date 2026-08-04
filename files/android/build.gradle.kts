8246e71dd581b05fc2c2024bf03c6625c3e7dd37
Sagar Betapudi <sagarbetapudi@gmail.com>
2026-08-04 19:59:45 +0530

    Remove duplicate project files and temporary artifacts

--- files/android/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.protobuf") version "0.9.4" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
-}
